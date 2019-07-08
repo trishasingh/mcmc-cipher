d2f8c28d401583874eaf8ae45fc20d7944c3f0a1
trishasingh <tsingh@middlebury.edu>
2019-07-08 16:25:02 +0530

    reading input, making transition matrix

---
+++ code/classes.py
@@ -0,0 +1,11 @@
+class CodingFunc:
+
+    def __init__(self, mapping):
+        self.mapping = mapping
+
+    def encrypt(self, mapping):
+        d = self.mapping
+        
+
+# x = CodingFunc({'a':'b', 'z':'y'})
+# print(x.mapping)
---
+++ code/testPipeline.py
@@ -0,0 +1,15 @@
+tst_input = """
+hamlet to be or not to be that is the question whether 
+tis nobler in the mind to suffer the slings and arrows 
+of outrageous fortune or to take arms against a sea of troubles 
+and by opposing end
+"""
+
+# Input
+tst = """
+Hamlet:   To be, or not to be: that is the question:
+Whether 'tis nobler in the mind to suffer
+The slings and arrows of outrageous fortune,
+Or to take arms against a sea of troubles,
+And by opposing end
+"""

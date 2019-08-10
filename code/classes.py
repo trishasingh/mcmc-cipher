ce87b90330a0669d85ab36c80e37a9a49ba627db
trishasingh <tsingh@middlebury.edu>
2019-08-11 00:20:14 +0530

    Transitions class instead of corpus, methods WIP

--- code/classes.py
+++ code/classes.py
@@ -18,18 +18,37 @@ class CodingFunc:
         d = self.mapping
 
 
-class Corpus:
+class Transitions:
 
     def __init__(self, text):
-        # original text
-        self.text = text
-        # transitions map
-        transitions = defaultdict(dict)
-        prev = self.text[0]
-        for item in self.text[1:]:
-            transitions[prev][item] = transitions[prev].setdefault(item, 0) + 1
+        # transition counts
+        count = defaultdict(dict)
+        prev = text[0]
+        for item in text[1:]:
+            count[prev][item] = count[prev].setdefault(item, 0) + 1
             prev = item
-        self.transitions = transitions
+        self.count = count
+
+    def transposition(self, old, new):
+        # steps: copy over dictionary, check key and swap
+        # copy over inner dict, check key and swap 
+        # TODO: fix naming in main.py, diff name for transitions
+
+        count = self.count.copy()
+        new_count = defaultdict(dict)
+        for k,v in count.items():
+            new_k = k
+            if k == old:
+                new_k = new
+            if k == new:
+                new_k == old 
+            for k1,v1 in v.items():
+                if k1 == old:
+                    v[k1] = new 
+                if k1 == new:
+                    v[k1] = old
+        return(count)
+
 
 
 # x = CodingFunc({'a':'b', 'z':'y'})
@@ -37,6 +56,7 @@ class Corpus:
 
 # TODO: how to handle if message doesnt have chars found in map
 
-x = Corpus("i am so happy today yay")
-print(x.text)
-print(x.transitions)
+x = "i am so happy today yay"
+x_transitions = Transitions(x)
+print(x_transitions.count)
+print(x_transitions.transposition('i', 'a'))
